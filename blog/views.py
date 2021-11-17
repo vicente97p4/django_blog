a4a548cf3930c2b2882588c8c0d00da1dc6cdae9
vicente97p4 <vicente97p4@naver.com>
2021-11-17 16:44:38 +0900

    Create single_post_page with FBV

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
+
+
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
@@ -10,5 +12,17 @@ def index(request):
         'blog/index.html',
         {
             'posts': posts,
+        }
+    )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk) # pk 필드 값이 pk인 Post 레코드를 가져오라는 의미이다.
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' : post
         }
     )
