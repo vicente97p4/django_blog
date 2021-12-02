b5f285344b89eec737e6b4451f77c47ad527ce6d
vicente97p4 <yees1104@naver.com>
2021-12-02 14:49:50 +0900

    comments in post detail page

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 # Create your tests here.
 # 테스트를 진행할 때 이미 운영되고 있는 서버의 DB를 건드리면 곤란하므로 가상 DB를 만들고 진행한다.
@@ -46,6 +46,12 @@ class TestView(TestCase):
         )
         self.post_003.tags.add(self.tag_python_kor)
         self.post_003.tags.add(self.tag_python)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_001,
+            content='첫 번째 댓글입니다.'
+        )
 
     def category_card_test(self, soup):
         categories_card = soup.find('div', id='categories-card')
@@ -157,6 +163,12 @@ class TestView(TestCase):
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
+        # comment area
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
 
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
