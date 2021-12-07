09c458066ade2bb03d7c7637dfe942fa2abb529f
vicente97p4 <yees1104@naver.com>
2021-12-07 15:17:36 +0900

    user avatar

--- blog/models.py
+++ blog/models.py
@@ -75,3 +75,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://picsum.photos/seed/{self.author.pk}/60/60'
