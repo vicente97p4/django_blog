dabd72e7d5fc3b42f3f45f9eb77118bc4a019e99
vicente97p4 <yees1104@naver.com>
2021-12-16 12:52:17 +0900

    board

---
+++ board/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Board
+
+# Create your views here.
+class BoardList(ListView):
+    model = Board
+    ordering = '-pk'
+
+
+class BoardDetail(DetailView):
+    model = Board
+
+
+class BoardCreate(LoginRequiredMixin, CreateView):
+    model = Board
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            response = super(BoardCreate, self).form_valid(form)
+
+            return response
+        else:
+            redirect('/board/')
--- django_blog_prj/settings.py
+++ django_blog_prj/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
     'blog',
     'single_pages',
+    'board',
 ]
 
 MIDDLEWARE = [
